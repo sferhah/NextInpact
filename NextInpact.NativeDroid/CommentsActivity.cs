61eb6b7fbc6ab7486adfa198e556abbfcfa583ee
Sami FERHAH <ferhah@outlook.com>
2017-07-10 01:36:05 +0200

    CommentsActivity

---
+++ NextInpact.NativeDroid/CommentsActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using NextInpact.Core.ViewModels;
+
+namespace NextInpact.NativeDroid
+{
+    [Activity(Label = "NextInpact.NativeDroid", MainLauncher = false, Icon = "@drawable/logo_nextinpact")]
+    public class CommentsActivity : MvxAppCompatActivity<CommentsViewModel>
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.activity_liste_commentaires);            
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            if (base.ViewModel.Items.Count == 0)
+                base.ViewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
+
---
+++ NextInpact.NativeDroid/Converters/StringToHtmlConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.Views;
+using System.Globalization;
+using MvvmCross.Platform.Converters;
+using Android.Text;
+
+namespace NextInpact.NativeDroid.Converters
+{
+    public class StringToHtmlConverter : MvxValueConverter<String, ISpanned>
+    {
+        protected override ISpanned Convert(String strHtml, Type targetType, object parameter, CultureInfo cultureInfo)
+        {
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.Build.VERSION_CODES.N)
+                return Html.FromHtml(strHtml, Html.FromHtmlModeLegacy);
+
+            return Html.FromHtml(strHtml);
+        }
+    }
+}
