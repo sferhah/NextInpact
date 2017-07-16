c8681d23b9513cc248826d50d8109654e98faab3
Sami FERHAH <ferhah@outlook.com>
2017-07-16 19:08:53 +0200

    Forms.Droid : Splash screen

--- NextInpact.Forms.Android/MainActivity.cs
+++ NextInpact.Forms.Android/MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using MvvmCross.Core.ViewModels;
 using MvvmCross.Core.Views;
+using MvvmCross.Forms.Core;
 using MvvmCross.Forms.Droid;
 using MvvmCross.Forms.Droid.Presenters;
 using MvvmCross.Platform;
@@ -9,7 +11,7 @@ using NextInpact.Forms;
 
 namespace NextInpact.Droid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : MvxFormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -17,18 +19,15 @@ namespace NextInpact.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+            base.OnCreate(bundle);          
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);         
-       
             var presenter = Mvx.Resolve<IMvxViewPresenter>() as MvxFormsDroidPagePresenter;
             presenter.MvxFormsApp = new App();
-            LoadApplication(presenter.MvxFormsApp);
+            LoadApplication(presenter.MvxFormsApp);            
 
             //Needs to be set after 'LoadApplication'
             Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
         }
-
 
     }
 }
--- NextInpact.Forms.Android/SplashScreen.cs
+++ NextInpact.Forms.Android/SplashScreen.cs
@@ -1,12 +1,14 @@
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
 using MvvmCross.Droid.Views;
+using Xamarin.Forms;
 
 namespace NextInpact.Droid
 {
     [Activity(
         Label = "NextInpact.Droid"
-        , MainLauncher = false
+        , MainLauncher = true
         , Icon = "@mipmap/icon"
         , Theme = "@style/Theme.Splash"
         , NoHistory = true
@@ -14,8 +16,33 @@ namespace NextInpact.Droid
     public class SplashScreen : MvxSplashScreenActivity
     {
         public SplashScreen()
-            : base(Resource.Layout.SplashScreen)
+               : base(Resource.Layout.SplashScreen)
         {
+        }
+
+        private bool isInitializationComplete = false;
+        public override void InitializationComplete()
+        {
+            if (!isInitializationComplete)
+            {
+                isInitializationComplete = true;
+                StartActivity(typeof(MainActivity));
+            }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+            Xamarin.Forms.Forms.ViewInitialized += (object sender, ViewInitializedEventArgs e) =>
+            {
+                if (!string.IsNullOrWhiteSpace(e.View.StyleId))
+                {
+                    e.NativeView.ContentDescription = e.View.StyleId;
+                }
+            };
+
+            base.OnCreate(bundle);
         }
     }
 }
